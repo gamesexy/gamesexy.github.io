df9d3609fb86f99b69786805ef9d9d18bbe01955
gamesexy <gamedevx@outlook.com>
2022-02-22 07:20:53 +0800

    Site updated: 2022-02-22 07:20:52

--- js/randombg.js
+++ js/randombg.js
@@ -1,9 +1,15 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-  "url(/img/bg1.jpg)",
-  "url(/img/bg2.jpg)",
-  "url(/img/bg3.jpg)",
-  "url(/img/bg4.jpg)"
+  "url(/img/backimg/bg1.jpg)",
+  "url(/img/backimg/bg2.jpg)",
+  "url(/img/backimg/bg3.jpg)",
+  "url(/img/backimg/bg4.jpg)",
+  "url(/img/backimg/bg5.jpg)",
+  "url(/img/backimg/bg6.jpg)",
+  "url(/img/backimg/bg7.jpg)",
+  "url(/img/backimg/bg8.jpg)",
+  "url(/img/backimg/bg9.jpg)",
+  "url(/img/backimg/bg10.jpg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex =Math.floor(Math.random() * backimg.length);
@@ -11,10 +17,16 @@ var bgindex =Math.floor(Math.random() * backimg.length);
 document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
 //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var bannerimg =[
-  "url(/img/bg1.jpg)",
-  "url(/img/bg2.jpg)",
-  "url(/img/bg3.jpg)",
-  "url(/img/bg4.jpg)"
+  "url(/img/backimg/bg1.jpg)",
+  "url(/img/backimg/bg2.jpg)",
+  "url(/img/backimg/bg3.jpg)",
+  "url(/img/backimg/bg4.jpg)",
+  "url(/img/backimg/bg5.jpg)",
+  "url(/img/backimg/bg6.jpg)",
+  "url(/img/backimg/bg7.jpg)",
+  "url(/img/backimg/bg8.jpg)",
+  "url(/img/backimg/bg9.jpg)",
+  "url(/img/backimg/bg10.jpg)"
 ];
 //获取banner图片总数，生成随机数
 var bannerindex =Math.floor(Math.random() * bannerimg.length);
